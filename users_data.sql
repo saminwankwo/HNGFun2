738d96f26ea9c14f6c13b52f4e3089bfd21df0a6
DEREKDUNES <mbahderek@gmail.com>
2018-04-29 18:06:38 +0100

    derekdunes adding user_data.sql for team dragon shield

---
+++ users_data.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 29, 2018 at 07:01 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dragon_shield`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_data`
+--
+
+CREATE TABLE `users_data` (
+  `user_id` int(10) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `phonenumber` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `nationality` varchar(50) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `reset_link` varchar(255) NOT NULL,
+  `reset_link_expiry` varchar(255) NOT NULL,
+  `image_filename` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users_data`
+--
+ALTER TABLE `users_data`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users_data`
+--
+ALTER TABLE `users_data`
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
